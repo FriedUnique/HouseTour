98954bd96d78244508e0c07793b3b97a76bfd50f
Stefan Ochsner GWM <stefano.ochsner@yahoo.com>
2026-03-09 21:18:15 +0100

    Added Model view

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 
+
 export default class Room {
-    constructor(centerX, centerY, height, width, texture, divId) {
+    constructor(centerX, centerY, height, width, tpPosition, texture, divId) {
         this.centerX = centerX;
         this.centerY = centerY;
         this.height = height;
         this.width = width;
         this.texture = texture;
-        this.divId = divId
+        this.divId = divId;
+        this.tpPosition = tpPosition;
     }
 
     get picturePath(){
         return this.texture;
     }
 
-    isClickInRoom(x, y){
-        leftPoint = this.centerX-(0.5*width);
-        topPoint = this.centerY-(0.5*height);
-        checkWidth = (leftPoint+width) <! x
-        checkHeight = (topPoint+height) <! y
-        return checkHeight && checkWidth;
-    }
 
 }
+
